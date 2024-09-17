7dded2dde4939863ba99486dad2f67840a5fd824
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-17 08:05:01 +0200

    Completed AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+    <main>
+        <div class="wrapper">
+            <h2>Content</h2>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    min-height: 125px;
+    background-color: #000;
+
+    h2 {
+        color: #FFF;
+        line-height: 125px;
+        text-align: center;
+    }
+}
+</style>
